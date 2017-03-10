116bfbc3a8aa60bddccf96806269ce57ab973c2d
Alexander Young <alexander.p.young@gmail.com>
2017-03-10 17:55:09 +0000

    Externalised uploads from container

--- setup.sh
+++ setup.sh
@@ -6,12 +6,13 @@ fi
 
 echo API_NAME: $API_NAME
 
+mkdir -p /data/taiga-media
 mkdir -p /data/postgres
 
 docker run -d --name postgres  -v /data/postgres:/var/lib/postgresql/data postgres
 # postgres needs some time to startup
 sleep 5
-docker run -d --name taiga-back  -p 8008:8000 -e API_NAME=$API_NAME  --link postgres:postgres lxndryng/taiga-back
+docker run -d --name taiga-back  -p 8000:8000 -e API_NAME=$API_NAME  -v /data/taiga-media:/usr/src/app/taiga-back/media --link postgres:postgres lxndryng/taiga-back
 docker run -d --name taiga-front -p 80:80 -e API_NAME=$API_NAME --link taiga-back:taiga-back --volumes-from taiga-back lxndryng/taiga-front
 
 docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createuser -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -d -r -s taiga'"
